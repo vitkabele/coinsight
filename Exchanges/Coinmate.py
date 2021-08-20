5d7a90c1b2d3af6f44ba3a25d5e116a1f99b831d
Vit Kabele <vit@kabele.me>
2021-08-20 22:43:05 +0200

    Add cli options for output customization Let the summary results be sorted by date

--- Exchanges/Coinmate.py
+++ Exchanges/Coinmate.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read(filename: str) -> pd.DataFrame:
-    df = pd.read_csv(filename, sep=';', header=0, parse_dates=[1], infer_datetime_format=True,
+    df = pd.read_csv(filename, sep=';', header=0, parse_dates=['Date'],
                      usecols=['Date', 'Type', 'Amount', 'Amount Currency', 'Price', 'Price Currency', 'Fee',
                               'Fee Currency'])
     df['Exchange'] = 'CoinMate'
--- main.py
+++ main.py
@@ -11,21 +11,38 @@ def sort_df_cols(df: pd.DataFrame) -> pd.DataFrame:
 def print_stats(df: pd.DataFrame) -> None:
     grouped = df.groupby('Amount Currency')['Amount'].sum()
     sumtotal = df.groupby('Price Currency')['Total'].sum()
-    print("Holdings:")
     print(grouped.add(sumtotal, fill_value=0).round(10))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Unify the format of crypto exchange logs')
-    parser.add_argument('input_file', type=str, help='Input file with data from supported exchange', nargs='+')
+    parser.add_argument('input_files', type=str, help='Input file with data from supported exchange', nargs='+')
+    parser.add_argument('-s', '--stats', action='store_true', help='Print various statistics for input data')
+    parser.add_argument('-i', '--intermediate', action='store_true', help='Store processed output for each input file')
+    parser.add_argument('-O', '--output', help='Output file', default='summary_results.csv')
     args = parser.parse_args()
+    results = []
 
     for i in args.input_file:
         if i.startswith('bitstamp'):
             data = read_bitstamp(i)
         elif i.startswith('coinmate'):
             data = read_coinmate(i)
+        else:
+            print(f"Unknown file {i}")
+            continue
 
-        print_stats(data)
-        data.pipe(sort_df_cols).to_csv(f'{i}.processed.csv', index=False, date_format='%Y-%m-%d %H:%M', decimal=',', sep=';')
+        results.append(data)
 
+        if args.stats:
+            print(f'Results for {i}:')
+            print_stats(data)
+
+        if args.intermediate:
+            data.pipe(sort_df_cols).to_csv(f'{i.removesuffix(".csv")}.processed.csv', index=False, date_format='%Y-%m-%d %H:%M', decimal=',', sep=';')
+
+    combined_results = pd.concat(results).sort_values(by='Date')
+    if args.stats:
+        print('Summary statistics:')
+        print_stats(combined_results)
+    combined_results.pipe(sort_df_cols).to_csv(args.output, index=False, date_format='%Y-%m-%d %H:%M', decimal=',', sep=';')
